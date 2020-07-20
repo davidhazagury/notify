42606ef646aa39302e6419ff2f3ab9f1c7b7b840
David Hernandez Azagury <davidhazagury@gmail.com>
2020-07-20 16:55:29 +0200

    Ajax implemented, sidebar has been rendered

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,4 +1,15 @@
 class FoldersController < ApplicationController
+  # Shows all the folders
   def index
+    @folders = Folder.all
+  end
+
+  def show
+    @folder = Folder.find(params[:id])
+  end
+
+  def new
+  end
+  def create
   end
 end
