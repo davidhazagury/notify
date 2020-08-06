24f8bbcc186c98dc4aeec355b15b6ee8e76bf014
David Hernandez Azagury <davidhazagury@gmail.com>
2020-08-06 08:06:05 +0200

    FEAT: Added redirection to first folder when user is logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if current_user
+      first_folder = current_user.folders.first
+      redirect_to folder_notes_path(first_folder)
+    end
   end
 end
